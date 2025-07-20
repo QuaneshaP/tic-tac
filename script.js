a600cb3e3c5318100ba39394784e3a4afcfea5b2
Nesha <quaneshapryor2@gmail.com>
2025-07-20 12:19:43 -0400

    Revised the code to work correctly

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
-function Gameboard(){
-   const rows = 3;
-   const columns = 3;
-   const board = [];
-
-   for(let i = 0; i < rows; i++ ){
-      board[i] = [];
-      for(let j = 0; j < columns; j++){
-         board[i].push(Grid());
-      }
-   }
-
-   const getBoard = () => board;
-}
-
-function GameController (
-    playerOne = "Player One",
-    playerTwo = "Player Two"
-){
-   const players = [
-      {
-         name: playerOne,
-         token: "X"
-      },
-      {
-         name: playerTwo,
-         token: "O"
-      }
-   ]
-
-   let activePlayer = players[0];
-
-   const switchPlayer = () =>{
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-   };
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    
+    for (let i = 0; i < rows; i++) {
+        const inner = [];
+        for(let j = 0; j < columns; j++){
+            inner.push([]);
+        }
+      board.push(inner);    
+    }
+    
+    const dropToken = (row, column, token) => {
+         board[row][column].push(token);
+    }
    
-   const getActivePlayer = () => activePlayer;
+    
+    const getBoard = () => board;
 
-   return {getActivePlayer, players};
+    const printBoard = () => {
+      console.log(board);
+    }
+    
+    return {dropToken, getBoard, printBoard };
+    
+    
+    
 }
+function GameController (
+    playerOneName = "Player One", 
+    playerTwoName = "Player Two") {
 
-function Grid(){
-   let value = "";
+   const board = Gameboard();
+    
+    const players = [
+        { name: playerOneName,
+          token: "X"
+        },
+        { name: playerTwoName,
+         token: "O"
+        }
+    ];
 
-   const addMarker = (player) => {
-      value = player;
-   }
+    let activePlayer = players[0];
 
-   const getValue = () => value;
+    const switchPlayers = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
 
-   return {
-      addMarker,
-      getValue
-   };
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = () =>{
+      console.log(`Adding ${getActivePlayer().name}'s ${getActivePlayer().token}to the board`);
+      board.dropToken(1, 2, getActivePlayer().token);
+
+      switchPlayers();
+      
+      board.getBoard();
+      board.printBoard();
+
+    }
+
+    return {
+      playRound,
+      getActivePlayer
+    };
+    
 }
+const newGame = GameController();
+console.log(newGame);
