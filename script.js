ebefc2af3935961251f2d45d7f47ed795dc8e039
Nesha <quaneshapryor2@gmail.com>
2025-07-22 20:50:56 -0400

    Added the game logic that works in the console

--- script.js
+++ script.js
@@ -49,23 +49,75 @@ function GameController (
     };
 
     const getActivePlayer = () => activePlayer;
+    
 
     const playRound = () =>{
       console.log(`Adding ${getActivePlayer().name}'s ${getActivePlayer().token}to the board`);
-      board.dropToken(1, 2, getActivePlayer().token);
+      let choiceRow = prompt(`${getActivePlayer().name} Select the row!`);
+      let choiceCol = prompt(`${getActivePlayer().name} Select the column!`)
 
-      switchPlayers();
+      if (isNaN(choiceCol) || isNaN(choiceRow) ){
+        alert("Please only enter numbers");
+      }
+
+      board.dropToken(choiceRow, choiceCol, getActivePlayer().token);
+
+      //Check for a winner
+      const checkWin = (token) =>{
+          let winCheck = board.getBoard();
+
+          const winningMatches = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]]
+          ];
+          
+          for(const combos of winningMatches){
+            const [a, b, c] = combos;
+
+
+            if(winCheck[a[0]][a[1]][0] === token &&
+               winCheck[b[0]][b[1]][0] === token &&
+               winCheck[c[0]][c[1]][0] === token 
+            ) {
+              return true;
+            }
+          }
+          return false;
+      };
+
+      const winner = checkWin(getActivePlayer().token);
+
+      if(winner) {
+        console.log(`And the winner is :${getActivePlayer().name} congratulations!`);
+        return; 
+      }
+     
       
+      
+      switchPlayers();
+
       board.getBoard();
       board.printBoard();
+
+      if(!winner){
+        playRound();
+      }
+
 
     }
 
     return {
       playRound,
-      getActivePlayer
+      getActivePlayer,
+      
     };
     
 }
-const newGame = GameController();
-console.log(newGame);
+const game = GameController();
+console.log(game.playRound());
