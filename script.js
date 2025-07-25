b1825ccf3469284c73bdad17d28c1489cb367966
Nesha <quaneshapryor2@gmail.com>
2025-07-24 21:02:25 -0400

    Added a UI and reworked it to work in the dom. xxx

--- script.js
+++ script.js
@@ -13,10 +13,7 @@ function Gameboard() {
     
     const dropToken = (row, column, token) => {
          board[row][column].push(token);
-         if (board[row][column].length !== 0 ){
-          alert("Please add your token into an empty space.");
-          return;
-         }
+         
     }
    
     
@@ -48,24 +45,39 @@ function GameController (
 
     let activePlayer = players[0];
 
+    const getActivePlayer = () => activePlayer;
+
     const switchPlayers = () => {
       activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
 
-    const getActivePlayer = () => activePlayer;
+    
     
 
     const playRound = () =>{
-      console.log(`Adding ${getActivePlayer().name}'s ${getActivePlayer().token} to the board`);
-      let choiceRow = prompt(`${getActivePlayer().name} Select the row!`);
-      let choiceCol = prompt(`${getActivePlayer().name} Select the column!`)
-
-      if (isNaN(choiceCol) || isNaN(choiceRow) ){
-        alert("Please only enter numbers");
-      }
-
-      board.dropToken(choiceRow, choiceCol, getActivePlayer().token);
-
+      domManuipulation.banner.innerHTML = "";
+      domManuipulation.firstBtn.addEventListener("click", function(){
+        domManuipulation.banner.innerHTML = `Adding ${getActivePlayer().name}'s ${getActivePlayer().token} to the board`;
+        board.dropToken(0, 0, getActivePlayer().token);
+        domManuipulation.firstBtn.innerHTML = "";
+        domManuipulation.firstBtn.innerHTML = `${getActivePlayer().token}`
+        switchPlayers();
+      });
+      domManuipulation.secondBtn.addEventListener("click", function(){
+        domManuipulation.banner.innerHTML = `Adding ${getActivePlayer().name}'s ${getActivePlayer().token} to the board`;
+        board.dropToken(0, 1, getActivePlayer().token);
+         domManuipulation.secondBtn.textContent = "";
+        domManuipulation.secondBtn.textContent = `${getActivePlayer().token}`
+        switchPlayers();
+      });
+      domManuipulation.thirdBtn.addEventListener("click", function(){
+        domManuipulation.banner.innerHTML = `Adding ${getActivePlayer().name}'s ${getActivePlayer().token} to the board`;
+        board.dropToken(0, 2, getActivePlayer().token);
+        domManuipulation.thirdBtn.textContent = "";
+        domManuipulation.thirdBtn.textContent = `${getActivePlayer().token}`
+        switchPlayers();
+      });
+      
       //Check for a winner
       const checkWin = (token) =>{
           let winCheck = board.getBoard();
@@ -80,6 +92,7 @@ function GameController (
             [[0, 0], [1, 1], [2, 2]],
             [[0, 2], [1, 1], [2, 0]]
           ];
+          
           
           for(const combos of winningMatches){
             const [a, b, c] = combos;
@@ -96,16 +109,26 @@ function GameController (
       };
 
       const winner = checkWin(getActivePlayer().token);
+      let player1_Score = 0;
+      let player2_Score = 0;
 
       if(winner) {
         console.log(`And the winner is :${getActivePlayer().name}! Congratulations!`);
         board.printBoard();
+        if(getActivePlayer().name === "Player One"){
+          ++player1_Score;
+          domManuipulation.scoreBoard.textContent = `Player One: ${player1_Score} Player Two: ${player2_Score}`;
+        }
+        else if(getActivePlayer().name === "Player Two"){
+          ++player2_Score;
+          domManuipulation.scoreBoard.textContent = `Player One: ${player1_Score} Player Two: ${player2_Score}`
+        }
         return; 
       }
      
       
       
-      switchPlayers();
+      
 
       board.getBoard();
       board.printBoard();
@@ -116,13 +139,27 @@ function GameController (
 
 
     }
-
+    
+    const domManuipulation = {
+      scoreBoard: document.querySelector(".score"),
+      firstBtn: document.querySelector(".first"),
+      secondBtn: document.querySelector(".second"),
+      thirdBtn: document.querySelector(".third"),
+      fourthBtn: document.querySelector(".fourth"),
+      fifthBtn: document.querySelector(".fifth"),
+      sixBtn: document.querySelector(".six"),
+      sevenBtn: document.querySelector(".seven"),
+      eightBtn: document.querySelector(".eight"),
+      nineBtn: document.querySelector(".nine"),
+      banner: document.querySelector(".banner")
+    }
     return {
       playRound,
       getActivePlayer,
       
     };
     
+    
 }
 const game = GameController();
-console.log(game.playRound());
+game.playRound();
